e74cb0ea7ebb542cbff0a63ab1a57eaadf2e7a84
Akshat-developer14 <codeprogramme123@gmail.com>
2025-09-25 12:59:26 +0530

    strings in python

---
+++ strings/strings_in_py.py
@@ -0,0 +1,25 @@
+'''
+
+1. Definition
+- A string is a sequence of characters enclosed in single ('...'), double ("..."), or triple quotes (\'''...\''' or \"""...\""").
+- Python has no separate char type; even a single character is a string of length 1.
+
+'''
+
+s1 = 'Hello'
+s2 = "World"
+s3 = '''Multi-line
+string'''
+s4 = """Another
+multi-line
+string"""
+
+print('s1: ',s1)  # Hello
+print('s2: ',s2)  # World
+print('s3: ',s3)  # Multi-line
+                  # string
+print('s4: ',s4)  # Another
+                  # multi-line
+                  # string
+
+
