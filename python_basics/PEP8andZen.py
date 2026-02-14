b0c72d1f9aae46835964a6f5b74d5eaed2b60448
Akshat-developer14 <akshat.dev.tech@gmail.com>
2026-02-14 17:29:33 +0530

    Understanding PEP8 and ZEN of python

---
+++ python_basics/PEP8andZen.py
@@ -0,0 +1,12 @@
+'''
+Docstring for python_basics.PEP8 and Zen
+
+PEP8 - Is the convention for writing python code, such as 
+    4 spaces and never tabs howeever code editors do this job by themselves.
+    Use good names such as meaningful names instead of unclear short forms.
+    Formating nothing also done by code editors.
+
+Zen - In terminal open python using python3 in macos and linux, python for windows
+    Type - import this
+    it will provide with some pythonic way of writing code.
+'''
--- python_working/dis_module.py
+++
@@ -1,8 +0,0 @@
-import dis
-
-def add(a, b):
-    return a + b
-
-print(add(10, 5))
-
-dis.dis(add)
