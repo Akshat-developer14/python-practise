70f882294ac7a4fe328bb4a0c0c38f8d81f97830
Akshat-developer14 <akshat.dev.tech@gmail.com>
2026-02-14 17:40:06 +0530

    Understanding Python objects and mutability

---
+++ python_basics/ObjectsAndMutability/objects.py
@@ -0,0 +1,10 @@
+'''
+Docstring for python_basics.ObjectsAndMutability.objects
+
+Objects - Everything is object in python
+    Every object has an identity, it's type and it's value
+
+Mutable and Immutable - 
+    Mutable means that is changeable
+    Immutable means that is not changeable
+'''
