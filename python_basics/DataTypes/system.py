bb1ce2b057e1b63cf3367f5a9d5f8cf99f446aad
Akshat-developer14 <akshat.dev.tech@gmail.com>
2026-02-15 20:41:12 +0530

    Understanding system info of python

---
+++ python_basics/DataTypes/system.py
@@ -0,0 +1,15 @@
+import sys
+
+ideal_temp = 30.6
+current_temp = 23.3
+
+print(f'''
+Ideal temperature: {ideal_temp}
+Ideal temperature: {current_temp}
+
+Difference between temperature: {ideal_temp - current_temp}
+
+Printing some information of float:-
+{sys.float_info}
+''')
+
