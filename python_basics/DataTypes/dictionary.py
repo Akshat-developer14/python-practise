442e30b81078bab3b730976069eff9d0596aa8aa
Akshat-developer14 <codeprogramme123@gmail.com>
2026-03-01 00:17:25 +0530

    Implementing dictionary instead of match case

--- python_basics/DataTypes/dictionary.py
+++ python_basics/DataTypes/dictionary.py
@@ -59,3 +59,22 @@ print(f"\nGet value of spices: {chai_recipe.get('spices')}")
 # Clear dictionary
 chai_recipe.clear()
 print(f"\nChai recipe after clearing: {chai_recipe}")
+
+# Dictionary in place of match case
+users = [
+{"id": 1, "total": 100, "coupon": "P20"},
+{"id": 2, "total": 150, "coupon": "F10"},
+{"id": 3, "total": 80, "coupon": "P50"},
+]
+
+discounts = {
+"P20": (0.2, 0),
+"F10": (0.5, 0),
+"P50": (0, 10),
+}
+
+print('\n----- Coupon Discount -----\n')
+for user in users:
+    percent, fixed = discounts.get(user["coupon"], (0, 0))
+    discount = user["total"] * percent + fixed
+    print(f"{user["id"]} paid {user["total"]} and got discount for next visit of rupees {discount}")
